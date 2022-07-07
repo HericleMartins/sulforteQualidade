9a2903358497287de66ad537c806efa97bee1b5e
HericlesMartins <91044238+HericlesMartins@users.noreply.github.com>
2022-07-07 16:17:28 -0300

    indices

--- model/OrdemProducaoBobina.class.php
+++ model/OrdemProducaoBobina.class.php
@@ -95,7 +95,6 @@ class OrdemProducaoBobina {
     }
     public static function quantidadeBobinasNaoAnalisadas($dataInicial,$dataFinal) {
         global $sql;
-        $data = Date('Y-m-d');
         $sql->ExecuteSQL("Select (SUM(quantBobinaPesada) - sum(quantRegistro)) as quantidadeBobinasNaoAnalisadas from(
             SELECT
               idordemProducao,
@@ -140,51 +139,45 @@ class OrdemProducaoBobina {
     public static function bobinasNaoAnalisadas($idUsuario,$dataInicial,$dataFinal) {
         global $sql;
         $sql->ExecuteSQL("SELECT * from(
-            SELECT
-              numero,
-              maquina,
-              quantBobinaPesada,
-              item,
-              cliente,
-              bobina,
-              dataCriacao,
-              (SELECT
-                COUNT(id) AS id
-              FROM (SELECT
-                idextrusoraAnalise AS id,
-                idordemProducao AS idop
-              FROM sulforte.dbo.extrusoraAnalise
-              ) AS A
-              WHERE (idop = C.idordemProducao))
-              AS quantRegistro
+          SELECT
+            numero,
+            maquina,
+            quantBobinaPesada,
+            item,
+            cliente,
+            (SELECT
+              COUNT(id) AS id
             FROM (SELECT
-              O.idordemProducao,
-              O.numero,
-              O.item,
-              c.cliente,
-              B.maquina,
-              M.idmaquina,
-              B.numero as bobina,
-              B.dataCriacao,
-              COUNT(B.idordemProducaoBobina) AS quantBobinaPesada
-            FROM sulforte.dbo.ordemProducao AS O
-            INNER JOIN sulforte.dbo.ordemProducaoBobina AS B
-              ON B.idordemProducao = O.idordemProducao inner join
-               sulforte.dbo.cliente c on c.idcliente = O.idcliente
-            LEFT OUTER JOIN sulforte.dbo.maquina AS M
-              ON M.maquina = B.maquina
-              AND M.idtipoMaquina = 1
-            WHERE o.idordemProducao IN (
-                    SELECT opb.IDORDEMPRODUCAO FROM sulforte.DBO.ordemProducaoBobina opb WHERE CAST(opb.dataCriacao AS DATE) >= '$dataInicial' AND  CAST(opb.dataCriacao AS DATE) <= '$dataFinal'
-                    ) or O.idordemProducao in ( SELECT vAT.IDORDEMPRODUCAO FROM SULFORTE.DBO.viewareaTrabalho vAT where vat.idtipoMaquina = 1)
-            GROUP BY O.idordemProducao,
-                     O.numero,
-                     B.maquina,
-                     M.idmaquina,
-                     O.item,
-                     C.cliente,
-                     B.numero,
-                     B.dataCriacao ) AS C) as G where G.quantBobinaPesada - G.quantRegistro > 0");
+              idextrusoraAnalise AS id,
+              idordemProducao AS idop
+            FROM sulforte.dbo.extrusoraAnalise
+            ) AS A
+            WHERE (idop = C.idordemProducao))
+            AS quantRegistro
+          FROM (SELECT
+            O.idordemProducao,
+            O.numero,
+            O.item,
+            c.cliente,
+            B.maquina,
+            M.idmaquina,
+            COUNT(B.idordemProducaoBobina) AS quantBobinaPesada
+          FROM sulforte.dbo.ordemProducao AS O
+          INNER JOIN sulforte.dbo.ordemProducaoBobina AS B
+            ON B.idordemProducao = O.idordemProducao inner join
+             sulforte.dbo.cliente c on c.idcliente = O.idcliente
+          LEFT OUTER JOIN sulforte.dbo.maquina AS M
+            ON M.maquina = B.maquina
+            AND M.idtipoMaquina = 1
+          WHERE o.idordemProducao IN (
+                  SELECT opb.IDORDEMPRODUCAO FROM sulforte.DBO.ordemProducaoBobina opb WHERE CAST(opb.dataCriacao AS DATE) >= '$dataInicial' AND  CAST(opb.dataCriacao AS DATE) <= '$dataFinal'
+                  ) or O.idordemProducao in ( SELECT vAT.IDORDEMPRODUCAO FROM SULFORTE.DBO.viewareaTrabalho vAT where vat.idtipoMaquina = 1)
+          GROUP BY O.idordemProducao,
+                   O.numero,
+                   B.maquina,
+                   M.idmaquina,
+                   O.item,
+                   C.cliente ) AS C) as G where G.quantBobinaPesada - G.quantRegistro > 0");
         if ($sql->numRows() > 0) {
             return $sql->arrayResults();
         }
